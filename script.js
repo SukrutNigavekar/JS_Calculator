42fc2c8f4fad6fd54a652e3e7a466e5008663847
SukrutNigavekar <86665705+SukrutNigavekar@users.noreply.github.com>
2023-06-23 22:23:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function one(){
+    document.getElementById("textbox").value += "1";
+}
+function two(){
+    document.getElementById("textbox").value += "2";
+}
+function three(){
+    document.getElementById("textbox").value += "3";
+}
+function four(){
+    document.getElementById("textbox").value += "4";
+}
+function five(){
+    document.getElementById("textbox").value += "5";
+}
+function six(){
+    document.getElementById("textbox").value += "6";
+}
+function seven(){
+    document.getElementById("textbox").value += "7";
+}
+function eight(){
+    document.getElementById("textbox").value += "8";
+}
+function nine(){
+    document.getElementById("textbox").value += "9";
+}
+function zero(){
+    document.getElementById("textbox").value += "0";
+}
+function plus(){
+    document.getElementById("textbox").value += "+";
+}
+function minus(){
+    document.getElementById("textbox").value += "-";
+}
+function multiply(){
+    document.getElementById("textbox").value += "*";
+}
+function divide(){
+    document.getElementById("textbox").value += "/";
+}
+function answer(){
+    document.getElementById("textbox").value = eval(document.getElementById("textbox").value);
+}
+function decimal(){
+    document.getElementById("textbox").value += ".";
+}
+function clearText(){
+    document.getElementById("textbox").value = "";
+}
